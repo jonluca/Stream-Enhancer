6be22f7c3a0a37f3434d7ff1f4f17b740e2f016e
JonLuca DeCaro <jdecaro@usc.edu>
2018-09-29 14:56:01 -0400

    Better spacing

--- js/StreamEnhancer.js
+++ js/StreamEnhancer.js
@@ -9,12 +9,10 @@ function requestMonitor(request) {
       // playlist link
       let b64request = btoa(JSON.stringify(request));
       let url = "streamenhancer://" + b64request;
-      chrome.devtools.inspectedWindow.eval(`const win = window.open("${url}", '_blank');
-  win.focus();`);
       shouldContinueToMonitor = false;
       $("#scan").removeClass("scanning");
       $(".loader").css('display', 'none');
-      $("body").append(`<a class="link" href="${url}" id="link" role="button"><span>Link</span></a>`);
+      $("#results").append(`<a class="link" href="${url}" id="link" role="button"><span>Link</span></a>`);
     }
     console.log("got request in should");
   }
